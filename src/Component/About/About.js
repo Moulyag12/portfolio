5a9e60274b13d62457ed41bd4c40f863437eb087
Moulya <moulyagreddy@gmail.com>
2025-06-29 09:23:10 +0530

    about section update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -5,10 +5,10 @@ function About() {
     <h1 className='text'>About Me</h1>
    <div className="about-text" id="about">
 
-  Hi there! I'm Moulya G, a passionate Passionate Software Engineer 
-  with 2 years of experience specializing in building scalable systems
+  Hi there! I'm Moulya G, a passionate Software Engineer with
+  2 years of experience specializing in building scalable systems
   and intuitive user interfaces. Proficient in Java, Spring Boot, 
-  Angular, and JavaScript, with a strong focus on optimizing performance 
+  Angular and JavaScript, with a strong focus on optimizing performance 
   and delivering real-time data processing solutions across distributed
   applications. Proven ability to contribute to significant improvements 
   in application performance, reduce manual errors, and enhance system 
